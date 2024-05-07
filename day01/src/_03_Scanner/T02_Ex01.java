9dc431c9d0d5efe56f56d61092a58803efe97b1e
SO YEONG <soyeong27399@gmail.com>
2024-05-07 18:36:04 +0900

    04

--- day01/src/_03_Scanner/T02_Ex01.java
+++ day01/src/_03_Scanner/T02_Ex01.java
@@ -5,7 +5,21 @@ public class T02_Ex01 {
 	public static void main(String[] args) {
 		// 국어, 컴퓨터, 수학 점수를 입력받아 저장하고
 		// 총점과 평균을 출력하세요		
+		
+		System.out.print("국어점수를 입력하세요 : ");
+		
+		
+		
+		System.out.print("컴퓨터점수를 입력하세요 : ");
+		
+		
+		
+		
+		System.out.print("수학점수를 입력하세요 : ");
 
+		
+		
+		
 	}
 
 }
