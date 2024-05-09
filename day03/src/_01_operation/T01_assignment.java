6828a6c2cb87b5573c22276679841eeb46fc28bb
SO YEONG <soyeong27399@gmail.com>
2024-05-09 13:00:10 +0900

    dsa

---
+++ day03/src/_01_operation/T01_assignment.java
@@ -0,0 +1,10 @@
+package _01_operation;
+
+public class T01_assignment {
+
+	public static void main(String[] args) {
+		// 
+
+	}
+
+}
---
+++ day03/src/_01_operation/T02_increase.java
@@ -0,0 +1,10 @@
+package _01_operation;
+
+public class T02_increase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
